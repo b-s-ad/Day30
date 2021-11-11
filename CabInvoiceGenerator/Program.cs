c34319255bbba7dd4b7e5f6a308c0a0fcb6313de
b-s-ad <ahmedshehbazb@gmail.com>
2021-11-11 23:34:04 +0530

    CabInvoiceGenerator

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n***********************************\nWelcome To Cab Invoice Generator\n***********************************\n");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(0.1, 5); //tc2
+            double fare2 = invoiceGenerator.CalculateFare(2.0, 5);//tc1 //tc2
+            Console.WriteLine($"Fare : {fare}");
+            Console.WriteLine($"Fare2 : {fare2}");
+            Console.ReadLine();
+        }
+    }
+}
